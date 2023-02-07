840dce02cefdd53d7bd730aecba5aaede92e478e
JaykeBird <jacobrh94@hotmail.com>
2023-02-06 23:09:14 -0600

    Add project files.

---
+++ WpfPackUriErrorRepro/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace WpfPackUriErrorRepro
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var bim = new BitmapImage(new Uri("pack://application:,,,/SmileyColor.png", UriKind.RelativeOrAbsolute));
+            Debug.Print("Smiley image is " + bim.PixelWidth + " wide.");
+        }
+    }
+}
